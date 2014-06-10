80b9c200f9eff1297fb53e254c375edf258e6422
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2014-06-10 18:37:55 +0200

    Simplified GET_MEMBER_POINTER_MACRO. Use a DEFAULT_RETURN_VALUE macro instead of two variants of GET_MEMBER_POINTER

--- include/resultset.h
+++ include/resultset.h
@@ -26,10 +26,10 @@ if (i >= tupel->itemLen) { \
 } \
 valuePtr = tupelVar->items[i]->value; \
 childName += strlen(tupelVar->items[i]->name); \
+if ((dm = getDescription(rootDatamodel,tupelVar->items[i]->name)) == NULL) { \
+	return returnValue; \
+} \
 if (strlen(childName) > 0) { \
-	if ((dm = getDescription(rootDatamodel,tupelVar->items[i]->name)) == NULL) { \
-		return returnValue; \
-	} \
 	if ((tokInput = ALLOC(strlen(childName) + 1)) == NULL) { \
 		return returnValue; \
 	} \
@@ -54,46 +54,58 @@ if (strlen(childName) > 0) { \
 	FREE(tokInput); \
 }
 
-#define GET_MEMBER_POINTER(tupelVar,rootDatamodel,typeName)	int ret = 0, i = 0; \
-DataModelElement_t *dm = NULL; \
+#define GET_MEMBER_POINTER_ALGO_ONLY(tupelVar, rootDatamodelVar, typeName, datamodelElementVar, valuePtrVar) int ret = 0, i = 0; \
 char *token = NULL, *tokInput = NULL, *childName = NULL; \
-void *valuePtr = NULL; \
-for (i = 0; i < tupel->itemLen; i++) { \
+for (i = 0; i < tupelVar->itemLen; i++) { \
 	if ((childName = strstr(typeName,tupelVar->items[i]->name)) != NULL) { \
 		break; \
 	} \
 } \
-if (i >= tupel->itemLen) { \
-	return; \
+if (i >= tupelVar->itemLen) { \
+	return DEFAULT_RETURN_VALUE; \
 } \
-valuePtr = tupelVar->items[i]->value; \
+valuePtrVar = tupelVar->items[i]->value; \
 childName += strlen(tupelVar->items[i]->name); \
-if ((dm = getDescription(rootDatamodel,tupelVar->items[i]->name)) == NULL) { \
-	return; \
+if ((datamodelElementVar = getDescription(rootDatamodelVar,tupelVar->items[i]->name)) == NULL) { \
+	return DEFAULT_RETURN_VALUE; \
 } \
 if (strlen(childName) > 0) { \
 	if ((tokInput = ALLOC(strlen(childName) + 1)) == NULL) { \
-		return; \
+		return DEFAULT_RETURN_VALUE; \
 	} \
 	strcpy(tokInput,childName); \
 	token = strtok(tokInput,"."); \
 	while (token) { \
-		if ((ret = getOffset(dm,token)) == -1) { \
-			return; \
+		if ((ret = getOffset(datamodelElementVar,token)) == -1) { \
+			return DEFAULT_RETURN_VALUE; \
 		} \
-		valuePtr = valuePtr + ret; \
-		for(i = 0; i < dm->childrenLen; i++) { \
-			if (strcmp(dm->children[i]->name,token) == 0) { \
+		valuePtrVar = valuePtrVar + ret; \
+		for(i = 0; i < datamodelElementVar->childrenLen; i++) { \
+			if (strcmp(datamodelElementVar->children[i]->name,token) == 0) { \
 				break; \
 			} \
 		} \
-		if (i >= dm->childrenLen) { \
-			return; \
+		if (i >= datamodelElementVar->childrenLen) { \
+			return DEFAULT_RETURN_VALUE; \
 		} \
-		dm = dm->children[i]; \
+		datamodelElementVar = datamodelElementVar->children[i]; \
 		token = strtok(NULL,"."); \
 	} \
 	FREE(tokInput); \
+}
+
+#define GET_MEMBER_POINTER(tupelVar,rootDatamodel,typeName) DataModelElement_t *dm = NULL; \
+void *valuePtr = NULL; \
+GET_MEMBER_POINTER_ALGO_ONLY(tupelVar, rootDatamodel, typeName, dm, valuePtr)
+
+#define GET_TYPE_FROM_DM(varName,varType) if (varName->dataModelType == SOURCE) { \
+	varType = ((Source_t*)varName->typeInfo)->returnType; \
+} else if (varName->dataModelType == EVENT) { \
+	varType = ((Event_t*)varName->typeInfo)->returnType; \
+} else if (varName->dataModelType == OBJECT) { \
+	varType = ((Object_t*)varName->typeInfo)->identifierType; \
+} else { \
+	varType = varName->dataModelType; \
 }
 
 typedef struct __attribute__((packed)) Item {
@@ -108,51 +120,68 @@ typedef struct __attribute__((packed)) Tupel {
 	Item_t **items;
 } Tupel_t;
 
-static inline void setItemInt(DataModelElement_t *rootDM, Tupel_t *tupel, char *typeName, int value) {	
+static inline void setItemInt(DataModelElement_t *rootDM, Tupel_t *tupel, char *typeName, int value) {
+	#define DEFAULT_RETURN_VALUE	
 	GET_MEMBER_POINTER(tupel,rootDM,typeName);
 	*(int*)valuePtr = value;
+	#undef DEFAULT_RETURN_VALUE
 }
 
 static inline void setItemByte(DataModelElement_t *rootDM, Tupel_t *tupel, char *typeName, char value) {
+	#define DEFAULT_RETURN_VALUE	
 	GET_MEMBER_POINTER(tupel,rootDM,typeName);
 	*(char*)valuePtr = value;
+	#undef DEFAULT_RETURN_VALUE
 }
 
 static inline void setItemFloat(DataModelElement_t *rootDM, Tupel_t *tupel, char *typeName, double value) {
+	#define DEFAULT_RETURN_VALUE	
 	GET_MEMBER_POINTER(tupel,rootDM,typeName);
 	*(double*)valuePtr = value;
+	#undef DEFAULT_RETURN_VALUE
 }
 
 static inline void setItemString(DataModelElement_t *rootDM, Tupel_t *tupel, char *typeName, char *value) {
+	#define DEFAULT_RETURN_VALUE
 	GET_MEMBER_POINTER(tupel,rootDM,typeName);
 	if (IS_COMPACT(tupel)) {
 		DEBUG_MSG(1,"Refusing access (%s) to an item, because to tupel is compact.\n",__FUNCTION__);
 		return;
 	}
 	*(PTR_TYPE*)valuePtr = (PTR_TYPE)value;
+	#undef DEFAULT_RETURN_VALUE
 }
 
 static inline int getItemInt(DataModelElement_t *rootDM, Tupel_t *tupel, char *typeName) {
-	GET_MEMBER_POINTER_RETURN(tupel,rootDM,typeName,0);
+	#define DEFAULT_RETURN_VALUE 0
+	GET_MEMBER_POINTER(tupel,rootDM,typeName);
 	return *(int*)valuePtr;
+	#undef DEFAULT_RETURN_VALUE
 }
 
 static inline char getItemByte(DataModelElement_t *rootDM, Tupel_t *tupel, char *typeName) {
-	GET_MEMBER_POINTER_RETURN(tupel,rootDM,typeName,'\0');
+	#define DEFAULT_RETURN_VALUE '\0'
+	GET_MEMBER_POINTER(tupel,rootDM,typeName);
 	return *(char*)valuePtr;
+	#undef DEFAULT_RETURN_VALUE
 }
 
 static inline double getItemFloat(DataModelElement_t *rootDM, Tupel_t *tupel, char *typeName) {
-	GET_MEMBER_POINTER_RETURN(tupel,rootDM,typeName,0);
+	#define DEFAULT_RETURN_VALUE 0
+	GET_MEMBER_POINTER(tupel,rootDM,typeName);
 	return *(double*)valuePtr;
+	#undef DEFAULT_RETURN_VALUE
 }
 
 static inline char* getItemString(DataModelElement_t *rootDM, Tupel_t *tupel, char *typeName) {
-	GET_MEMBER_POINTER_RETURN(tupel,rootDM,typeName,NULL);
+	#define DEFAULT_RETURN_VALUE NULL
+	GET_MEMBER_POINTER(tupel,rootDM,typeName);
 	return (char*)*(PTR_TYPE*)valuePtr;
+	#undef DEFAULT_RETURN_VALUE
 }
 
 static inline void setItemArray(DataModelElement_t *rootDM, Tupel_t *tupel, char *typeName, int num) {
+	#define DEFAULT_RETURN_VALUE
 	int size = 0;
 	GET_MEMBER_POINTER(tupel,rootDM,typeName);
 	size = getSize(rootDM,dm);
@@ -162,68 +191,85 @@ static inline void setItemArray(DataModelElement_t *rootDM, Tupel_t *tupel, char
 	}
 	*((PTR_TYPE*)valuePtr) = (PTR_TYPE)ALLOC(num * size + sizeof(int));
 	*(int*)(*((PTR_TYPE*)valuePtr)) = num;
-	DEBUG_MSG(2,"Allocated %ld@%p bytes for array %s\n",num * size + sizeof(int),(void*)*((PTR_TYPE*)valuePtr),dm->name);
+	DEBUG_MSG(2,"Allocated %ld@%p bytes for array %s\n",(long)(num * size + sizeof(int)),(void*)*((PTR_TYPE*)valuePtr),dm->name);
+	#undef DEFAULT_RETURN_VALUE
 }
 
 static inline void setArraySlotByte(DataModelElement_t *rootDM,Tupel_t *tupel,char *arrayTypeName,int arraySlot,char value) {
+	#define DEFAULT_RETURN_VALUE
 	GET_MEMBER_POINTER(tupel,rootDM,arrayTypeName);
 	if (arraySlot >= *(int*)(*(PTR_TYPE*)valuePtr)) {
 		return;
 	}
 	*(char*)((*(PTR_TYPE*)(valuePtr)) + sizeof(int) + arraySlot * SIZE_BYTE) = value;
+	#undef DEFAULT_RETURN_VALUE
 }
 
 static inline void setArraySlotInt(DataModelElement_t *rootDM,Tupel_t *tupel,char *arrayTypeName,int arraySlot,int value) {
+	#define DEFAULT_RETURN_VALUE
 	GET_MEMBER_POINTER(tupel,rootDM,arrayTypeName);
 	if (arraySlot >= *(int*)(*(PTR_TYPE*)valuePtr)) {
 		return;
 	}
 	*(int*)((*(PTR_TYPE*)(valuePtr)) + sizeof(int) + arraySlot * SIZE_INT) = value;
+	#undef DEFAULT_RETURN_VALUE
 }
 
 static inline void setArraySlotFloat(DataModelElement_t *rootDM,Tupel_t *tupel,char *arrayTypeName,int arraySlot,double value) {
+	#define DEFAULT_RETURN_VALUE
 	GET_MEMBER_POINTER(tupel,rootDM,arrayTypeName);
 	if (arraySlot >= *(int*)(*(PTR_TYPE*)valuePtr)) {
 		return;
 	}
 	*(double*)((*(PTR_TYPE*)(valuePtr)) + sizeof(int) + arraySlot * SIZE_FLOAT) = value;
+	#undef DEFAULT_RETURN_VALUE
 }
 
 static inline void setArraySlotString(DataModelElement_t *rootDM,Tupel_t *tupel,char *arrayTypeName,int arraySlot,char *value) {
+	#define DEFAULT_RETURN_VALUE
 	GET_MEMBER_POINTER(tupel,rootDM,arrayTypeName);
 	if (arraySlot >= *(int*)(*(PTR_TYPE*)valuePtr) || IS_COMPACT(tupel)) {
 		return;
 	}
 	*(char**)((*(PTR_TYPE*)(valuePtr)) + sizeof(int) + arraySlot * SIZE_STRING) = value;
+	#undef DEFAULT_RETURN_VALUE
 }
 
 static inline char getArraySlotByte(DataModelElement_t *rootDM,Tupel_t *tupel,char *arrayTypeName,int arraySlot) {
-	GET_MEMBER_POINTER_RETURN(tupel,rootDM,arrayTypeName,'\0');
+	#define DEFAULT_RETURN_VALUE '\0'
+	GET_MEMBER_POINTER(tupel,rootDM,arrayTypeName);
 	if (arraySlot >= *(int*)(*(PTR_TYPE*)valuePtr)) {
 		return '\0';
 	}
 	return *(char*)((*(PTR_TYPE*)(valuePtr)) + sizeof(int) + arraySlot * SIZE_BYTE);
+	#undef DEFAULT_RETURN_VALUE
 }
 
 static inline int getArraySlotInt(DataModelElement_t *rootDM,Tupel_t *tupel,char *arrayTypeName,int arraySlot) {
-	GET_MEMBER_POINTER_RETURN(tupel,rootDM,arrayTypeName,0);
+	#define DEFAULT_RETURN_VALUE 0
+	GET_MEMBER_POINTER(tupel,rootDM,arrayTypeName);
 	if (arraySlot >= *(int*)(*(PTR_TYPE*)valuePtr)) {
 		return 0;
 	}
 	return *(int*)((*(PTR_TYPE*)(valuePtr)) + sizeof(int) + arraySlot * SIZE_INT);
+	#undef DEFAULT_RETURN_VALUE
 }
 
 static inline double getArraySlotFloat(DataModelElement_t *rootDM,Tupel_t *tupel,char *arrayTypeName,int arraySlot) {
-	GET_MEMBER_POINTER_RETURN(tupel,rootDM,arrayTypeName,0);
+	#define DEFAULT_RETURN_VALUE 0
+	GET_MEMBER_POINTER(tupel,rootDM,arrayTypeName);
 	return *(double*)((*(PTR_TYPE*)(valuePtr)) + sizeof(int) + arraySlot * SIZE_FLOAT);
+	#undef DEFAULT_RETURN_VALUE
 }
 
 static inline char* getArraySlotString(DataModelElement_t *rootDM,Tupel_t *tupel,char *arrayTypeName,int arraySlot) {
-	GET_MEMBER_POINTER_RETURN(tupel,rootDM,arrayTypeName,NULL);
+	#define DEFAULT_RETURN_VALUE NULL
+	GET_MEMBER_POINTER(tupel,rootDM,arrayTypeName);
 	if (arraySlot >= *(int*)(*(PTR_TYPE*)valuePtr)) {
 		return NULL;
 	}
 	return (char*)((*(PTR_TYPE*)(valuePtr)) + sizeof(int) + arraySlot * SIZE_STRING);
+	#undef DEFAULT_RETURN_VALUE
 }
 
 static inline int initTupel(Tupel_t **tupel,unsigned long long timestamp, int numItems) {
