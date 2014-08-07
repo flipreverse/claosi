31d9fc71979fb17e5b32a4d8d5d9171983577fbc
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2014-08-07 17:32:40 +0200

    Added examples for remote queries

--- Kbuild
+++ Kbuild
@@ -1,5 +1,6 @@
 ccflags-y := -I$(src)/../../include
-obj-m := slc-core.o slc-net.o slc-process.o
+obj-m := slc-core.o slc-net.o slc-process.o slc-remotequery.o
 slc-core-y := lib/kernel/libkernel.o lib/query.o lib/resultset.o lib/datamodel.o lib/api.o lib/liballoc.o lib/communication.o
 slc-net-y := provider/kernel/net.o
 slc-process-y := provider/kernel/process.o
+slc-remotequery-y := provider/kernel/remotequery.o
---
+++ provider/kernel/remotequery.c
@@ -0,0 +1,68 @@
+#include <linux/module.h>
+#include <linux/kprobes.h>
+#include <linux/if_ether.h>
+#include <datamodel.h>
+#include <query.h>
+#include <api.h>
+
+static Query_t queryDisplay;
+static EventStream_t displayStream;
+static Predicate_t xPosPred, yPosPred;
+static Filter_t posFilter;
+
+static void printResult(unsigned int id, Tupel_t *tupel) {
+	struct timespec time;
+	unsigned long long timeUS;
+	int x = 0, y = 0;
+
+	getnstimeofday(&time);
+	x = getItemInt(SLC_DATA_MODEL,tupel,"ui.eventType.xPos");
+	y = getItemInt(SLC_DATA_MODEL,tupel,"ui.eventType.yPos");
+	timeUS = (unsigned long long)time.tv_sec * (unsigned long long)USEC_PER_SEC + (unsigned long long)(time.tv_nsec/NSEC_PER_USEC);
+	printk("timeStart=%llu, timeEnd=%llu, id=%u, tuple=0x%p\n",tupel->timestamp,timeUS,tupel->id,tupel);
+	printk("processing duration: %llu us, query id: %u,xPos=%d, yPos=%d\n",timeUS - tupel->timestamp,id,x,y);
+	freeTupel(SLC_DATA_MODEL,tupel);
+}
+
+static void setupQueries(void) {
+	initQuery(&queryDisplay);
+	queryDisplay.onQueryCompleted = printResult;
+	queryDisplay.root = GET_BASE(displayStream);
+	INIT_EVT_STREAM(displayStream,"ui.display",0,NULL) /*GET_BASE(posFilter))
+	INIT_FILTER(posFilter,NULL,2)
+	ADD_PREDICATE(posFilter,0,xPosPred)
+	SET_PREDICATE(xPosPred,GEQ, STREAM, "ui.eventType.xPos", POD, "700")
+	ADD_PREDICATE(posFilter,1,yPosPred)
+	SET_PREDICATE(yPosPred,LEQ, STREAM, "ui.eventType.yPos", POD, "300")*/
+}
+
+int __init remotequery_init(void) {
+	int ret = 0;
+
+	setupQueries();
+
+	if ((ret = registerQuery(&queryDisplay)) < 0 ) {
+		ERR_MSG("Query registration failed: %d\n",-ret);
+		return -1;
+	}
+
+	return 0;
+}
+
+void __exit remotequery_exit(void) {
+	int ret = 0;
+
+	if ((ret = unregisterQuery(&queryDisplay)) < 0 ) {
+		ERR_MSG("Unregister queries failed: %d\n",-ret);
+	}
+
+	freeOperator(GET_BASE(displayStream),0);
+}
+
+module_init(remotequery_init);
+module_exit(remotequery_exit);
+
+MODULE_AUTHOR("Alexander Lochmann (alexander.lochmann@tu-dortmund.de)");
+MODULE_DESCRIPTION("");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.1");
