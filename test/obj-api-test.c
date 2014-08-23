5f46685237f803b4af555df87d7f1b76b1b3ef7e
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2014-08-23 11:06:29 +0200

    obj-api-test: added forgotten conversion from INIT_TYPE to INIT_COMPLEX_TYPE

--- test/obj-api-test.c
+++ test/obj-api-test.c
@@ -167,7 +167,7 @@ static void initDatamodel(void) {
 	INIT_PLAINTYPE(typeDataLen,"dataLength",typePacketType,BYTE)
 	//INIT_REF(typeSockRef,"socket",typePacketType,"process.process.sockets")
 
-	INIT_TYPE(typePacketType,"packetType",nsNet1,2)
+	INIT_COMPLEX_TYPE(typePacketType,"packetType",nsNet1,2)
 	ADD_CHILD(typePacketType,0,typeMacHdr);
 	ADD_CHILD(typePacketType,1,typeMacProt);
 	/*ADD_CHILD(typePacketType,2,typeNetHdr);
@@ -215,7 +215,7 @@ static void initDatamodel(void) {
 	
 	INIT_PLAINTYPE(typeXPos,"xPos",typeEventType,INT)
 	INIT_PLAINTYPE(typeYPos,"yPos",typeEventType,INT)
-	INIT_TYPE(typeEventType,"eventType",nsUI,2)
+	INIT_COMPLEX_TYPE(typeEventType,"eventType",nsUI,2)
 	ADD_CHILD(typeEventType,0,typeXPos)
 	ADD_CHILD(typeEventType,1,typeYPos)
 
